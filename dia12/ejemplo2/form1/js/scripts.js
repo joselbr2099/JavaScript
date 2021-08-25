8828dcf4f19f1f3f982467f2eb98e2e34673abd9
Jose Luis Blas Ralde <you@example.coilsm>
2021-08-25 18:24:24 -0400

    add dia12

---
+++ dia12/ejemplo2/form1/js/scripts.js
@@ -0,0 +1,10 @@
+// Empty JS for your own code to be here
+"use strict";
+
+function getCreds(){
+  let user=document.getElementById('exampleInputEmail1');
+  let pasd=document.getElementById('exampleInputEmail1');
+  if (user!="jose"){
+    alert("error de usuario");
+  }
+}
